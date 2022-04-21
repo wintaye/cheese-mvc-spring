c3e7a1159a2f9439113dee193a4831a4c665a84b
wintaye <41543902+wintaye@users.noreply.github.com>
2022-04-20 19:18:11 -0500

    Add files via upload
    uploaded remaining files

---
+++ build/src/main/java/org/LaunchCode/cheesemvcspring/CheeseMvcSpringApplication.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.cheesemvcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseMvcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseMvcSpringApplication.class, args);
+	}
+
+}
